1e79220460d9a7868bef16091bcc4889da9005f1
Feranmi Akinyele <akinyeleferanmi19@gmail.com>
2025-02-25 09:08:25 +0100

    product

---
+++ script/script.js
@@ -0,0 +1,6 @@
+function addprod(){
+    let visible = document.querySelector('.t-forms')
+    visible.style.display = 'block'
+    let overlay = document.querySelector('.t-overlay')
+    overlay.style.backgroundColor = '#00000033'
+}
