f632cb5b444441fa457e313a3ebfdf421194e88a
Feranmi Akinyele <akinyeleferanmi19@gmail.com>
2025-02-26 18:42:08 +0100

    product

--- script/script.js
+++ script/script.js
@@ -1,10 +1,81 @@
 // <<<<<<< HEAD
-function addprod(){
-    let visible = document.querySelector('.t-forms')
-    visible.style.display = 'block'
-    let overlay = document.querySelector('.t-overlay')
-    overlay.style.backgroundColor = '#00000033'
-}
+// function addprod(){
+//     let visible = document.querySelector('.t-forms')
+//     visible.style.display = 'block'
+//     let overlay = document.querySelector('.t-overlay')
+//     overlay.style.backgroundColor = '#00000033'
+// }
+$(document).ready(function(){
+    $('.t-btn').click(function(){
+        console.log('cl')
+        let overlay = $('.t-overlay')
+        overlay.stop(true,true).fadeIn(500)
+    })
+    $('#close_form').click(function(event){
+        event.preventDefault()
+        let overlay = $('.t-overlay')
+        overlay.stop(true,true).fadeOut(500)
+    })
+    $('#create_prod').click(function(event){
+       event.preventDefault()
+       console.log('vb')
+
+       let name = $('#name').val()
+       let name_err = $('#name_err')
+       let bulk = $('#bulk').val()
+       let bulk_err = $('#bulk_err')
+       let unit = $('#unit').val()
+       let unit_err= $('#unit_err')
+       let quantity = $('#quantity').val()
+       let quantity_err= $('#quantity_err')
+       let manDate = $('#man_date').val()
+       let manDate_err = $('#man_date_err')
+       let ex_date = $('#ex_date').val()
+       let ex_date_err = $('#ex_date_err')
+    
+       
+       if(name === ''){
+          name_err.text('*This Field is Required')
+       }else{
+        name_err.text('')
+       }
+
+       if(bulk === ''){
+        bulk_err.text('*This Field is Required')
+       }else{
+       bulk_err.text('')
+       }
+
+     if(unit === ''){
+        unit_err.text('*This Field is Required')
+     }else{
+      unit_err.text('')
+     }
+
+     if(quantity === ''){
+        quantity_err.text('*This Field is Required')
+     }else{
+      quantity_err.text('')
+     }
+
+     if(manDate === ''){
+        manDate_err.text('*This Field is Required')
+     }else{
+      manDate_err.text('')
+     }
+
+     if(ex_date === ''){
+        ex_date_err.text('*This Field is Required')
+     }else{
+      ex_date_err.text('')
+     }
+
+
+       
+       
+    })
+})
+
 // =======
 function handleSubmit(event){
     event.preventDefault()
